754324d34c39610dd44aeb89bb485569097aee83
21020285 <21020285@vnu.edu.vn>
2022-09-10 11:56:37 +0700

    hi

--- StudentManagement/Student.java
+++ StudentManagement/Student.java
@@ -1,36 +1,83 @@
 public class Student {
 
     // TODO: khai bao cac thuoc tinh cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai bao cac phuong thuc getter, setter cho Student
 
-    /**
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup() {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail() {
+        this.email = email;
+    }
+    /** define student infomation type
      * Constructor 1
      */
+
     Student() {
         // TODO:
+        name = "Student";
+        id = "000";
+        group = "K62CB";
+        email = "uet@vnu.edu.vn";
     }
 
-    /**
+    /** take infomation from student
      * Constructor 2
-     * @param n
-     * @param sid
-     * @param em
+     * @param n name
+     * @param sid id
+     * @param em email
      */
-    Student(String name, String id, String email) {
+
+    public Student(String name, String id, String email) {
         // TODO:
+        this.name = name;
+        this.id = id;
+        this.group = "K62CB";
+        this.email = email;
     }
 
-    /**
+    /** I don't know, maybe copy Student?
      * Constructor 3
-     * @param s
+     * @param s add like Sutdent
      */
     Student(Student s) {
         // TODO:
+        name = s.name;
+        id = s.id;
+        group = s.group;
+        email = s.email;
     }
 
     String getInfo() {
         // TODO:
-        return null; // xoa dong nay sau khi cai dat
+        return name + " - " + id + " - " + group + " - " + email;
     }
 }
--- StudentManagement/StudentManagement.java
+++ StudentManagement/StudentManagement.java
@@ -2,22 +2,86 @@ public class StudentManagement {
 
     // TODO: khai bao thuoc tinh students la array chua cac doi tuong thuoc lop Student (max. 100)
 
+    Student[] students = new Student[100];
+    // check whether 2 student have same class
+
     public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xoa dong nay sau khi cai dat
+        String per1 = s1.getGroup();
+        String per2 = s2.getGroup();
+        return per1.equals(per2);
     }
+    // add student into Student class
 
-    public void addStudent(Student newStudent){
-	 // TODO:
+    public void addStudent(Student newStudent) {
+	    // TODO:
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                students[i] = new Student(newStudent);
+                break;
+            }
+        }
     }
+    //return like what question required
 
     public String studentsByGroup() {
         // TODO:
-	return null;
+        //create a blank string to return as info need
+        StringBuilder res = new StringBuilder();
+        int studentNum = students.length;
+        boolean[] printStudent = new boolean[studentNum];
+
+        //create array to check whether that student print or not
+
+        int attend = 0;
+
+        // count the student in list as attend
+
+        for (Student student : students) {
+            if (student != null) {
+                attend++;
+            } else {
+                break;
+            }
+        }
+        for (int i = 0; i < attend; i++) {
+            if (!printStudent[i]) {
+                res.append(students[i].getGroup());
+                res.append("\n");
+                for(int j = i; j < attend; j++) {
+                    if(sameGroup(students[i], students[j])) {
+                        res.append(students[j].getInfo());
+                        res.append("\n");
+                        printStudent[i] = true;
+                    }
+                }
+            }
+        }
+
+	    return res.toString();
     }
+    //remove student from id infomation
 
     public void removeStudent(String id) {
         // TODO:
+        int studentNum = 0;
+        for (Student student : students) {
+            if (student != null) studentNum++;
+            else {
+                break;
+            }
+        }
+        // find the number of student in student array
+
+        for (int i = 0; i < studentNum; i++) {
+            if (id.equals(students[i].getId())) {
+                for (int j = i; j < studentNum - 1; j++) {
+                    students[j] = students[j + 1];
+                }
+            }
+        }
+        // find id student then put behind place to that id place then clear the last place
+        students[studentNum - 1] = null;
     }
 
     public static void main(String[] args) {
